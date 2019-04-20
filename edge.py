adf0918184b838ae7e4363dafb1c85bebaf1f11d
Fodor Levente <levilevi183@gmail.com>
2019-04-20 23:45:18 +0200

    Added edge.py for represenating a single weighted edge between two node

---
+++ edge.py
@@ -0,0 +1,19 @@
+# Author: Levente Fodor
+# Date: 2019.04.20
+# Language: Python 3
+
+'''
+    u: number of children in node_1
+    v: number of children in node_2
+    w: weight between two nodes
+
+    myEdge = Edge(0,1,10)
+'''
+
+class Edge:
+
+    def __init__(self,u,v,w):
+        self.verticles = [u,v,w]
+
+    def print_edges(self):
+        print(f"(u:{self.verticles[0]}, v:{self.verticles[1]}, w:{self.verticles[2]})")
